1035bc581cb1040fda2f198104d360891a085c28
Ryan Lopopolo <rjl@hyperbo.la>
2020-06-28 14:58:35 -0700

    Add script for adjusting dates on photos from DSLR
    We forgot to set the date on the fancy camera before shooting, which
    defaulted to 2014-01-01 on boot.

---
+++ scripts/adjust_sony_alpha_6000_wedding_photo_timestamps.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+
+# The below timestamps are derived from photo `DSC02476.JPG`, which has original
+# timestamp `2014-01-01T21:34:46-07:00` and shows Ryan's watch at
+# `2020-06-27T10:15:00-07:00`
+
+# incorrect timeline
+camera = datetime(2014, 1, 1, 21, 34, 46)
+# correct timeline
+watch = datetime(2020, 6, 27, 10, 15, 0)
+
+adjustment = watch - camera
+
+print(f"Original timestamp: {camera}\nCorrected timestamp: {watch}\nAdjustment: {adjustment}")
